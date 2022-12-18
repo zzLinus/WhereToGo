0c5fbd24d878cb13c73cc65aecacf46c3b6066c9
Zheng Zhang <lz5333885@gmail.com>
2022-12-18 20:52:37 +0800

    give up raylib-cpp due to lag version compile problem

--- src/main.cc
+++ src/main.cc
@@ -1,63 +1,70 @@
-#include <raylib-cpp.hpp>
+#include "raylib.h"
 
-#if defined(PLATFORM_WEB)
-#include <emscripten/emscripten.h>
-#endif
+int main(void) {
+  const int screenWidth = 800;
+  const int screenHeight = 450;
 
-//----------------------------------------------------------------------------------
-// Global Variables Definition
-//----------------------------------------------------------------------------------
-int screenWidth = 800;
-int screenHeight = 450;
+  InitWindow(screenWidth, screenHeight,
+             "raylib [core] example - 3d camera free");
 
-//----------------------------------------------------------------------------------
-// Module Functions Declaration
-//----------------------------------------------------------------------------------
-void UpdateDrawFrame(void); // Update and Draw one frame
+  Camera camera = {0};
+  camera.position = (Vector3){10.0f, 10.0f, 10.0f};
+  camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+  camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+  camera.fovy = 45.0f;
+  camera.projection = CAMERA_PERSPECTIVE;
 
-//----------------------------------------------------------------------------------
-// Main Enry Point
-//----------------------------------------------------------------------------------
-int main() {
-  // Initialization
-  //--------------------------------------------------------------------------------------
-  raylib::Window window(screenWidth, screenHeight,
-                        "raylib-cpp [core] example - basic window");
+  Vector3 cubePosition = {0.0f, 0.0f, 0.0f};
+  Vector2 cubeScreenPosition = {0.0f, 0.0f};
 
-#if defined(PLATFORM_WEB)
-  emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
-#else
+  SetCameraMode(camera, CAMERA_FREE); // Set a free camera mode
+
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
   //--------------------------------------------------------------------------------------
 
   // Main game loop
-  while (!window.ShouldClose()) // Detect window close button or ESC key
+  while (!WindowShouldClose()) // Detect window close button or ESC key
   {
-    UpdateDrawFrame();
+    // Update
+    //----------------------------------------------------------------------------------
+    UpdateCamera(&camera);
+
+    // Calculate cube screen space position (with a little offset to be in top)
+    cubeScreenPosition = GetWorldToScreen(
+        (Vector3){cubePosition.x, cubePosition.y + 2.5f, cubePosition.z},
+        camera);
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    BeginMode3D(camera);
+
+    DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+    DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+    EndMode3D();
+
+    DrawText("Enemy: 100 / 100",
+             (int)cubeScreenPosition.x - MeasureText("Enemy: 100/100", 20) / 2,
+             (int)cubeScreenPosition.y, 20, BLACK);
+    DrawText(
+        "Text is always on top of the cube",
+        (screenWidth - MeasureText("Text is always on top of the cube", 20)) /
+            2,
+        25, 20, GRAY);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
   }
-#endif
+
+  // De-Initialization
+  //--------------------------------------------------------------------------------------
+  CloseWindow(); // Close window and OpenGL context
+  //--------------------------------------------------------------------------------------
 
   return 0;
-}
-
-//----------------------------------------------------------------------------------
-// Module Functions Definition
-//----------------------------------------------------------------------------------
-void UpdateDrawFrame(void) {
-  // Update
-  //----------------------------------------------------------------------------------
-  // TODO: Update your variables here
-  //----------------------------------------------------------------------------------
-
-  // Draw
-  //----------------------------------------------------------------------------------
-  BeginDrawing();
-
-  ClearBackground(RAYWHITE);
-
-  DrawText("Congrats! You created your first raylib-cpp window!", 190, 200, 20,
-           LIGHTGRAY);
-
-  EndDrawing();
-  //----------------------------------------------------------------------------------
 }
